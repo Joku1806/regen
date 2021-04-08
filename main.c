0d90954987f60ce65322c16aaef4f2e78889b0fb
Jonas Kuß <jonaskuss@protonmail.com>
2021-04-08 18:51:36 +0200

    Main Testprogramm um Funktionalität erweitern

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "parser_defs.h"
+#include "parser.h"
+#include "generator.h"
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -11,6 +12,8 @@ int main(int argc, char** argv) {
     ParserState *state = parse_regex(regex);
     if (!state->invalid) {
         printf("%s ist ein syntaktisch richtiger Regex. Herzlichen Glückwunsch!\n", regex);
+        // einfach nur sehen ob es kompiliert
+        DFA* generated = generate_first_pass_from_parsed_regex(state);
         return 1;
     } else {
         printf("%s ist kein syntaktisch richtiger Regex.\n", regex);
