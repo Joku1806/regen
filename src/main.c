96cb61d9d6e1bf602007c1241b659154ce42bfdb
Jonas Kuß <jonaskuss@protonmail.com>
2021-05-17 11:21:13 +0200

    Ausgeben, dass ein NFA generiert wurde.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "parser.h"
 #include "generator.h"
 
@@ -14,6 +15,7 @@ int main(int argc, char** argv) {
         printf("%s ist ein syntaktisch richtiger Regex. Herzlichen Glückwunsch!\n", regex);
         // einfach nur sehen ob es kompiliert
         NFA* generated = generate_NFA_from_parsed_regex(state);
+        printf("Got back generated NFA starting at %p.\n", generated->start);
         return 1;
     } else {
         printf("%s ist kein syntaktisch richtiger Regex.\n", regex);
