a6e102195031da388a9787961d942de95bc1c1e5
Jonas Kuß <jonaskuss@protonmail.com>
2021-05-17 11:23:56 +0200

    Unterstützung von 2 neuen Operatoren:
    mod_multiple: 1 Zeichen oder mehr
    mod_optional: 0 oder 1 Zeichen
    
    Es werden den Operatoren neue Zeichen zugewiesen, die besser zu der Bedeutung passen.
    Außerdem wird die Grammatiktabelle jetzt als Blockliste angesehen, d.h. es gibt darin nur noch Einträge,
    die nicht von der Grammatik anerkannt werden. Außerdem ist es dadurch möglich, die Werte der Enums direkt in der Array-Adressierung zu verwenden,
    was das Hinzufügen von neuen Regeln vereinfacht und das Ganze 1000x lesbarer macht.

--- src/parser.c
+++ src/parser.c
@@ -3,31 +3,46 @@
 #include <string.h>
 #include "parser.h"
 
-unsigned char grammar_table[7][7] = {
-    {1, 0, 0, 0, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 0, 1, 1, 1, 1},
-    {1, 0, 0, 0, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1}};
+bool grammar_blocklist[TOKEN_COUNT][TOKEN_COUNT] = {
+    [block_open][block_close] = true,
+    [block_open][mod_optional] = true,
+    [block_open][mod_any] = true,
+    [block_open][mod_multiple] = true,
+    [block_open][mod_choice] = true,
+    [mod_optional][mod_optional] = true,
+    [mod_optional][mod_any] = true,
+    [mod_optional][mod_multiple] = true,
+    [mod_any][mod_optional] = true,
+    [mod_any][mod_any] = true,
+    [mod_any][mod_multiple] = true,
+    [mod_multiple][mod_optional] = true,
+    [mod_multiple][mod_any] = true,
+    [mod_multiple][mod_multiple] = true,
+    [mod_choice][block_close] = true,
+    [mod_choice][mod_optional] = true,
+    [mod_choice][mod_any] = true,
+    [mod_choice][mod_multiple] = true,
+    [mod_choice][mod_choice] = true,
+};
 
 ParserState *construct_parser_state(char *regex) {
     ParserState *state = malloc(sizeof(ParserState));
     state->tokens = NULL;
-    state->cleaned_regex = calloc(strlen(regex) + 1, sizeof(char));
+    state->regex = calloc(strlen(regex) + 1, sizeof(char));
     state->escape_active = 0;
     state->open_blocks = 0;
     state->invalid = 0;
     return state;
 }
 
-Token get_token_type(char token, int escape_active) {
+Token get_token_type(char token, bool escape_active) {
     if (escape_active) return character;
     if (token == '(') return block_open;
     if (token == ')') return block_close;
-    if (token == '*') return mod_multiple;
-    if (token == '+') return mod_choice;
+    if (token == '?') return mod_optional;
+    if (token == '*') return mod_any;
+    if (token == '+') return mod_multiple;
+    if (token == '|') return mod_choice;
     if (token == '\\') return mod_escape;
     if (token == ' ' || token == '\t' || token == '\n') return whitespace;
     return character;
@@ -36,6 +51,8 @@ Token get_token_type(char token, int escape_active) {
 char *get_token_description(Token token) {
     if (token == block_open) return "Block::open";
     if (token == block_close) return "Block::close";
+    if (token == mod_optional) return "Mod::optional";
+    if (token == mod_any) return "Mod::any";
     if (token == mod_multiple) return "Mod::multiple";
     if (token == mod_choice) return "Mod::choice";
     if (token == mod_escape) return "Mod::escape";
@@ -57,7 +74,7 @@ void Token_formatter(VLA *formatter, void *item) {
 ParserState *parse_regex(char *regex) {
     ParserState *state = construct_parser_state(regex);
     VLA *token_history = VLA_initialize(strlen(regex), sizeof(Token));
-    VLA_set_data_freeing_policy(token_history, persistent);
+    VLA_set_data_freeing_policy(token_history, immutable);
     VLA_set_item_formatter(token_history, Token_formatter);
 
     // Dummy-Element, damit man direkt am Anfang auf grammar_table zugreifen kann.
@@ -67,8 +84,8 @@ ParserState *parse_regex(char *regex) {
         if (token_history->length != 0) previous = VLA_binding_get_Token(token_history, -1);
         Token current = get_token_type(regex[idx], state->escape_active);
 
-        if (!grammar_table[previous][current] || (current == block_close && state->open_blocks == 0)) {
-            state->invalid = 1;
+        if (grammar_blocklist[previous][current] || (current == block_close && state->open_blocks == 0)) {
+            state->invalid = true;
             return state;
         }
 
@@ -76,14 +93,14 @@ ParserState *parse_regex(char *regex) {
         if (current == block_close) state->open_blocks--;
 
         if (current == mod_escape)
-            state->escape_active = 1;
+            state->escape_active = true;
         else
-            state->escape_active = 0;
+            state->escape_active = false;
 
         // Nur zu den Token hinzufügen wenn es syntaktisch wichtig ist
         if (current != mod_escape && current != whitespace) {
             size_t char_index = token_history->length / token_history->item_size;
-            state->cleaned_regex[char_index] = regex[idx];
+            state->regex[char_index] = regex[idx];
             VLA_append(token_history, &current, 1);
         }
     }
@@ -91,13 +108,12 @@ ParserState *parse_regex(char *regex) {
     // prüfen, ob am Ende noch Gruppen angefangen und dann nicht geschlossen wurden
     Token last = VLA_binding_get_Token(token_history, -1);
     if (state->open_blocks != 0 || state->escape_active || last == mod_choice) {
-        state->invalid = 1;
+        state->invalid = true;
         return state;
     }
 
-    printf("Parsed Tokens: ");
     VLA_print(token_history);
-    printf("Cleaned regex: %s\n", state->cleaned_regex);
+    debug("Cleaned: %s\n", state->regex);
 
     state->tokens = (Token *)token_history->data;
     VLA_free(token_history);
--- src/parser.h
+++ src/parser.h
@@ -2,43 +2,49 @@
 #define PARSER_H
 
 #include <stddef.h>
+#include <stdbool.h>
 #include "VLA.h"
 
 typedef enum {
     block_open = 0,
     block_close = 1,
-    mod_multiple = 2,
-    mod_choice = 3,
-    mod_escape = 4,
-    whitespace = 5,
-    character = 6,
+    mod_optional = 2,
+    mod_any = 3,
+    mod_multiple = 4,
+    mod_choice = 5,
+    mod_escape = 6,
+    whitespace = 7,
+    character = 8,
 } Token;
+
+#define TOKEN_COUNT 9
 
 typedef struct {
     Token* tokens;
-    char* cleaned_regex;
-    int escape_active;
+    char* regex;
     int open_blocks;
-    int invalid;
+    bool escape_active;
+    bool invalid;
 } ParserState;
 
 // beschreibt, welcher Token-Typ nach einem anderen Token-Typ kommen darf
-// grammar_table[block_open][block_close] z.B. sagt aus, ob ein gerade geöffneter Block
-// ohne Inhalt sofort wieder geschlossen werden darf.
-extern unsigned char grammar_table[7][7];
+// grammar_blocklist[block_open][block_close] z.B. sagt aus, ob ein gerade geöffneter Block
+// ohne Inhalt sofort wieder geschlossen werden darf. Die Kombinationen von Tokens, die nicht
+// erlaubt sein sollen, müssen auf true gesetzt werden.
+extern bool grammar_blocklist[TOKEN_COUNT][TOKEN_COUNT];
 
 ParserState* construct_parser_state();
 // Definiert ein Mapping von Buchstaben auf die einzelnen Token-Typen.
-// Sollte man also den von mir gewählten Regex-Dialekt (weil in FoSA so gemacht lol)
-// umändern wollen, dann muss man einfach nur die Vergleiche in dieser Funktion umschreiben,
+// Sollte man also den von mir gewählten Regex-Dialekt umändern wollen,
+// dann muss man einfach nur die Vergleiche in dieser Funktion umschreiben,
 // da alle anderen Funktionen nur mit den Enums arbeiten.
 // escape_active wird gebraucht, damit alles nach escape als character interpretiert wird.
-Token get_token_type(char token, int escape_active);
+Token get_token_type(char token, bool escape_active);
 char* get_token_description(Token token);
+// Schreibt den geparsten Regex und Token-Array in Postfix-Notation um,
+// um die Erstellung des NFA einfacher zu machen
+void postfixify(ParserState* state);
 // Versucht den Regex zu parsen und prüft, ob er syntaktisch richtig ist (semantisch ist nochmal ne Ecke schwerer)
 ParserState* parse_regex(char* regex);
-
-// Gibt alle Tokens im VLA auf stdin aus.
-void VLA_print_tokens(VLA* v);
 
 #endif
