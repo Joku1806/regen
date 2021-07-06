7876d14efd5484c080a9234d81c8f7ae79dbf76b
Jonas Kuß <jonaskuss@protonmail.com>
2021-07-07 01:15:32 +0200

    Kompakten NFA und Utility-Methoden dafür hinzufügen.
    Kompakt heißt in diesem Fall, dass die Zustände in einem Array statt einzeln auf dem Heap gespeichert werden.
    Dieser Unterschied sorgt hoffentlich dafür, dass für große NFAs schneller auf die Zustände zugegriffen werden kann.
    Ein noch viel größerer Pluspunkt ist, dass diese Abstraktion es viel leichter macht, mit dem NFA zu arbeiten bzw Algorithmen dafür zu implementieren.

--- src/NFA.c
+++ src/NFA.c
@@ -1,9 +1,11 @@
+#include <stdbool.h>
+#include <string.h>
 #include "NFA.h"
 #include "debug.h"
 
-NFA_Node *construct_NFA_Node(size_t *id) {
-    NFA_Node *new = malloc(sizeof(NFA_Node));
-    new->NFA_Edges = VLA_initialize(1, sizeof(NFA_Edge));
+Node *construct_NFA_Node(size_t *id) {
+    Node *new = malloc(sizeof(Node));
+    new->NFA_Edges = VLA_initialize(1, sizeof(Edge));
     new->id = *id;
     // debug("Created NFA Node z%u\n", *id);
 
@@ -11,7 +13,7 @@ NFA_Node *construct_NFA_Node(size_t *id) {
     return new;
 }
 
-void free_NFA_Node(NFA_Node *to_free) {
+void free_NFA_Node(Node *to_free) {
     // TODO: Überlegen, was eigentlich mit den erstellten Objekten aus construct_NFA_Edge() passieren soll,
     // nachdem sie in den VLA kopiert wurden. Einfach löschen?
     // Alternativ könnte man auch einen Weg finden, die Objekte direkt im VLA zu instanziieren, dann müsste man
@@ -21,8 +23,8 @@ void free_NFA_Node(NFA_Node *to_free) {
     free(to_free);
 }
 
-NFA_Edge *construct_NFA_Edge(char *matching, NFA_Node *to) {
-    NFA_Edge *new = malloc(sizeof(NFA_Edge));
+Edge *construct_NFA_Edge(char *matching, Node *to) {
+    Edge *new = malloc(sizeof(Edge));
     new->matching = matching;
     new->advance_to = to;
     return new;
@@ -35,28 +37,63 @@ NFA *construct_NFA() {
     return new;
 }
 
-void NFA_add_connection_between(NFA_Node *from, NFA_Node *to, char *matching) {
+Compact_NFA *construct_Compact_NFA(size_t number_of_nodes) {
+    Compact_NFA *new = malloc(sizeof(Compact_NFA));
+    new->nodes = calloc(number_of_nodes, sizeof(Compact_Node));
+    new->number_of_nodes = number_of_nodes;
+    new->start_node_index = 0;
+    new->stop_node_index = 1;
+
+    return new;
+}
+
+Compact_Node generate_Compact_Node(Node *from) {
+    Compact_Node new = {
+        .outgoing_edges = calloc(VLA_get_length(from->NFA_Edges), sizeof(Compact_Edge)),
+        .number_of_outgoing_edges = VLA_get_length(from->NFA_Edges),
+    };
+
+    return new;
+}
+
+Compact_Edge generate_Compact_Edge(Edge *from) {
+    Compact_Edge new = {
+        .matching = (uint8_t *)from->matching,
+        .match_length = strlen(from->matching),
+        .target_index = from->advance_to->id,
+    };
+
+    return new;
+}
+
+void NFA_add_connection_between(Node *from, Node *to, char *matching) {
     if (from == NULL || to == NULL) {
         warn("At least one of the nodes (at %p and %p) is NULL. Can't connect them with a new edge.\n", from, to);
         return;
     }
 
     debug("Now adding connection from z%u to z%u matching %s.\n", from->id, to->id, matching);
-    NFA_Edge *connection = construct_NFA_Edge(matching, to);
+    Edge *connection = construct_NFA_Edge(matching, to);
     VLA_append(from->NFA_Edges, connection, 1);
 }
 
-void NFA_add_empty_connection_between(NFA_Node *from, NFA_Node *to) {
-    NFA_add_connection_between(from, to, NULL);
+void NFA_add_empty_connection_between(Node *from, Node *to) {
+    char *empty = calloc(1, sizeof(char));
+    NFA_add_connection_between(from, to, empty);
 }
 
-NFA_Node *VLA_binding_get_NFA_Node(VLA *v, signed long idx) {
-    VLA_assert_item_size_matches(v, sizeof(NFA_Node));
-    return (NFA_Node *)VLA_get(v, idx);
+Node *VLA_binding_get_NFA_Node(VLA *v, signed long idx) {
+    VLA_assert_item_size_matches(v, sizeof(Node));
+    return (Node *)VLA_get(v, idx);
+}
+
+Node *VLA_binding_get_NFA_Node_Pointer(VLA *v, signed long idx) {
+    VLA_assert_item_size_matches(v, sizeof(Node *));
+    return *(Node **)VLA_get(v, idx);
 }
 
 void NFA_Node_formatter(VLA *formatter, void *item) {
-    NFA_Node *casted = (NFA_Node *)item;
+    Node *casted = (Node *)item;
     VLA_append(formatter, "z", 1);
 
     const int n = snprintf(NULL, 0, "%zu", casted->id);
@@ -66,7 +103,26 @@ void NFA_Node_formatter(VLA *formatter, void *item) {
     VLA_append(formatter, &buffer, n);
 }
 
-NFA_Edge *VLA_binding_get_NFA_Edge(VLA *v, signed long idx) {
-    VLA_assert_item_size_matches(v, sizeof(NFA_Edge));
-    return (NFA_Edge *)VLA_get(v, idx);
+void NFA_Node_Pointer_formatter(VLA *formatter, void *item) {
+    Node *casted = *(Node **)item;
+    VLA_append(formatter, "z", 1);
+
+    const int n = snprintf(NULL, 0, "%zu", casted->id);
+    char buffer[n + 1];
+    snprintf(buffer, n + 1, "%zu", casted->id);
+
+    VLA_append(formatter, &buffer, n);
+}
+
+Edge *VLA_binding_get_NFA_Edge(VLA *v, signed long idx) {
+    VLA_assert_item_size_matches(v, sizeof(Edge));
+    return (Edge *)VLA_get(v, idx);
+}
+
+void NFA_free(NFA *NFA, Node **nodes) {
+    for (size_t idx = 0; idx < NFA->number_of_nodes; idx++) {
+        debug("Now trying to free node at %p (id=%ld (supposed) vs %ld (real)).\n", nodes[idx], idx, nodes[idx]->id);
+        free_NFA_Node(nodes[idx]);
+    }
+    free(NFA);
 }
--- src/NFA.h
+++ src/NFA.h
@@ -3,33 +3,61 @@
 
 #include "VLA.h"
 
+typedef struct Node Node;
+typedef struct Edge Edge;
 typedef struct NFA NFA;
-typedef struct NFA_Node NFA_Node;
-typedef struct NFA_Edge NFA_Edge;
+typedef struct Compact_Node Compact_Node;
+typedef struct Compact_Edge Compact_Edge;
+typedef struct Compact_NFA Compact_NFA;
 
-struct NFA_Edge {
+struct Edge {
+    Node *advance_to;
     char *matching;
-    NFA_Node *advance_to;
 };
 
-struct NFA_Node {
+struct Node {
     VLA *NFA_Edges;
     size_t id;
 };
 
 struct NFA {
-    NFA_Node *start;
-    NFA_Node *stop;
+    Node *start;
+    Node *stop;
+    size_t number_of_nodes;
+};
+
+struct Compact_Edge {
+    uint8_t *matching;
+    size_t match_length;
+    size_t target_index;
+};
+
+struct Compact_Node {
+    Compact_Edge *outgoing_edges;
+    size_t number_of_outgoing_edges;
+};
+
+struct Compact_NFA {
+    Compact_Node *nodes;
+    size_t number_of_nodes;
+    size_t start_node_index;
+    size_t stop_node_index;
 };
 
 void NFA_Node_formatter(VLA *formatter, void *item);
-NFA_Node *VLA_binding_get_NFA_Node(VLA *v, signed long idx);
-NFA_Edge *VLA_binding_get_NFA_Edge(VLA *v, signed long idx);
-NFA_Node *construct_NFA_Node(size_t *id);
+void NFA_Node_Pointer_formatter(VLA *formatter, void *item);
+Node *VLA_binding_get_NFA_Node(VLA *v, signed long idx);
+Node *VLA_binding_get_NFA_Node_Pointer(VLA *v, signed long idx);
+Edge *VLA_binding_get_NFA_Edge(VLA *v, signed long idx);
+Node *construct_NFA_Node(size_t *id);
+Compact_Node generate_Compact_Node(Node *from);
+Compact_Edge generate_Compact_Edge(Edge *from);
 NFA *construct_NFA();
-NFA_Edge *construct_NFA_Edge(char *matching, NFA_Node *to);
-void NFA_add_connection_between(NFA_Node *from, NFA_Node *to, char *matching);
-void NFA_add_empty_connection_between(NFA_Node *from, NFA_Node *to);
-void free_NFA_Node(NFA_Node *to_free);
+Compact_NFA *construct_Compact_NFA(size_t number_of_nodes);
+Edge *construct_NFA_Edge(char *matching, Node *to);
+void NFA_add_connection_between(Node *from, Node *to, char *matching);
+void NFA_add_empty_connection_between(Node *from, Node *to);
+void free_NFA_Node(Node *to_free);
+void NFA_free(NFA *NFA, Node **nodes);
 
 #endif
